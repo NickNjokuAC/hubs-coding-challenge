8b46be822fd3d3b0e84187854909f96a24b25ead
NickNjokuAC <109548584+NickNjokuAC@users.noreply.github.com>
2022-09-13 16:42:58 -0500

    logo changes

--- src/utils/configs.js
+++ src/utils/configs.js
@@ -1,5 +1,5 @@
-import appLogo from "../assets/images/app-logo.png";
-import appLogoDark from "../assets/images/app-logo-dark.png";
+import appLogo from "../assets/images/NN Studios-logos_black.png";
+import appLogoDark from "../assets/images/NN Studios-logos_white.png";
 import companyLogo from "../assets/images/company-logo.png";
 import homeHeroBackground from "../assets/images/home-hero-background-unbranded.png";
 import sceneEditorLogo from "../assets/images/editor-logo.png";
